4c58e8b27ba09eeaeee9f7757d029f0ffba03672
vparent <v.parent215@laposte.net>
2020-05-21 10:39:34 +0200

    Update of xinitrc, alacritty, fish, picom, spectrwm and vim

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,3 +1,7 @@
+alias path 'echo $PATH | tr -s ":" "\n"'
+alias reload 'source $XDG_CONFIG_HOME/fish/config.fish $XDG_CONFIG_HOME/conf.d/*.fish'
+alias bat 'cat /sys/class/power_supply/BAT0/capacity'
+
 function ls
 	command ls --color=auto --group-directories-first $argv
 end
@@ -13,28 +17,42 @@ end
 function l
 	command ls -Cf --color=auto --group-directories-first $argv
 end
-alias ln 'command ln -v '
-alias mkdir 'command mkdir -pv '
-alias path 'echo $PATH | tr -s ":" "\n"'
-alias reload 'source $XDG_CONFIG_HOME/fish/config.fish $XDG_CONFIG_HOME/conf.d/*.fish'
+function ln 
+	command ln -v $argv
+end
+function mkdir 
+	command mkdir -pv $argv
+end
 function rm 
 	command rm -I $argv
 end
 function rmf
 	command rm -rfI $argv
 end
-alias view 'command view -R -c "syntax on"'
-alias objdump 'command objdump -M intel'
-alias wget 'command wget --hsts-file=$XDG_CACHE_HOME/wget-hsts'
-alias tmux 'command tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf'
-alias sqlite3 'command sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc'
-alias emacs 'command emacs --no-init-file --load=$XDG_CONFIG_HOME/emacs/config.el'
-alias feh 'command feh --no-fehbg'
-alias py 'command python'
-alias .. 'command cd ../'
-alias ... 'command cd ../../'
-alias d 'command dirs -v '
-alias bat 'cat /sys/class/power_supply/BAT0/capacity'
+function view 
+	command view -R -c "syntax on" $argv
+end
+function objdump
+	command objdump -M intel $argv
+end
+function wget
+	command wget --hsts-file=$XDG_CACHE_HOME/wget-hsts $argv
+end
+function tmux
+	command tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf $argv
+end
+function sqlite3
+	command sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc $argv
+end
+function feh
+	command feh --no-fehbg $argv
+end
+function py 
+	command python $argv
+end
+function d
+	command dirs -v $argv 
+end
 function src 
 	if test (pwd) = $HOME
 		cd .local/src
