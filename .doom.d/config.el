b03366d5d57451f691687b0c0f3b2a3e07599575
vparent <v.parent215@laposte.net>
2020-07-18 15:52:27 +0200

    Add multiple choice to xinit, change the fonts for alacritty, modify keybindings in spectrwm, change the modifier keybinding in tmux, change the company backend for c in doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -28,7 +28,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-vibrant)
 
 ; Set transparency for focused and unfocused frames
 (set-frame-parameter (selected-frame) 'alpha '(95 . 90))
@@ -68,9 +68,9 @@
   (set-company-backend! 'sh-mode
     '(company-shell :with company-yasnippet)))
 
-(after! cc-mode
-  (set-company-backend! 'c-mode
-    '(:separate company-irony-c-headers company-irony company-yasnippet)))
+;(after! cc-mode
+;  (set-company-backend! 'c-mode
+;    '(:separate company-irony-c-headers company-irony company-yasnippet)))
 
 (after! lsp-python-ms
   (set-lsp-priority! 'mspyls 1))
